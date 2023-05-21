79863b46a52061bf25f98db97470a4ca54bdcad6
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-05-21 14:59:11 -0300

    Update youtubeSearch.js

--- src/search/youtubeSearch.js
+++ src/search/youtubeSearch.js
@@ -8,7 +8,7 @@ async function youtubeSearch(message, YTB, sock, jid, msg, babytube){
     if(message?.startsWith("/playmp3 ")){
         const ytsearch = message.substr(9);
         const data = await YTB.search(ytsearch, { limit: 1 });
-        sock.sendMessage(jid, {image: {url: `${data[0].thumbnail.url}`}, mimetype: 'image/jpeg' , caption: `◆◇ musica: ${data[0].title}\n◆◇ canal: ${data[0].channel.name}\n◆◇ publicacao: ${data[0].uploadedAt}\n◆◇ duracao: ${data[0].durationFormatted}`},{quoted:msg })
+        sock.sendMessage(jid, {image: {url: `${data[0].thumbnail.url}`}, mimetype: 'image/jpeg' , caption: `◆◇ musica: ${data[0].title}\n◆◇ canal: ${data[0].channel.name}\n◆◇ publicacao: ${data[0].uploadedAt}\n◆◇ duracao: ${data[0].durationFormatted}\n*fazendo download... aguarde*`},{quoted:msg })
         babytube.musicDownload(`https://www.youtube.com/watch?v=${data[0].id}`, "./data-base/media/audios/music.mp3").on('finish', () => 
             sock.sendMessage( jid, { audio: { url: `./data-base/media/audios/music.mp3` }, mimetype: 'audio/mp4' , caption: `voce `})
         )
