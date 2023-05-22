30c6cb71f64a044f21af9ea23ea67aac42f26677
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-05-22 13:41:28 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get install python3
+apt-get install nodejs
+apt-get install libwebp
+yarn add axios fs pino @adiwajshing/baileys@4.4.0 dotenv gtts g-i-s @hapi/boom lodash moment-timezone child_process path babytube youtube-sr forever
+forever start --minUptime 1000 --spinSleepTime 1000 index.js
+echo "\n nexa inicializada"
