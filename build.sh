126e9a2025c1e19af4463f0f04ee3b5a9a47ec57
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-06-21 22:47:10 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+echo "installing libraries..."
+
+yarn add axios fs pino @adiwajshing/baileys@4.4.0 dotenv gtts g-i-s @hapi/boom lodash moment-timezone child_process path babytube youtube-sr qrcode-terminal
+
+npm install -g forever
+
+echo "installing necessary packages..." 
+
+apt-get install libwebp -y
+
+apt-get install python3 -y
+
+echo "starting service..."
+
+forever start --minUptime 1000 --spinSleepTime 1000 index.js
