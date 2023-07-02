6773c8e0277e78d8a65ba9a3748f3fe32bfa6f38
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-02 10:35:18 -0300

    Add files via upload

--- src/search/imgSearch.js
+++ src/search/imgSearch.js
@@ -1,16 +1,10 @@
-
-async function imgSearch(message, gis, sock, jid, msg) {
+async function imgSearch(message, GOOGLE_IMG_SCRAP, sock, jid, msg) {
     if(message?.startsWith("/imgsearch ")){
         const image = message.substr(11)
-        gis(image, logResults);
-        function logResults(error, results) {
-            if (error) {
-               sock.sendMessage(jid, {text: "imagem nao encontrada"}, {quoted:msg})
-            }
-            else {
-                sock.sendMessage(jid, {image: {url: `${results[0]?.url}`}, mimetype: 'image/jpeg' , caption: `principal resultado para ${image}`},{quoted:msg }).catch ( err => sock.sendMessage(jid, {text: "erro ao pesquisar imagem"}, {quoted:msg}))
-            }
-        }
+        const data = await GOOGLE_IMG_SCRAP({
+            search: image,
+        });
+        sock.sendMessage(jid, {image: {url: data.result[0].url}, mimetype: 'image/jpeg' , caption: `principal resultado para ${image}`},{quoted:msg }).catch ( err => sock.sendMessage(jid, {text: "erro ao pesquisar imagem"}, {quoted:msg}))
     }
 }
 module.exports = imgSearch;
