ef28416d3168c72d130e24833429d10c171ff730
Baby <89675173+Swag666baby@users.noreply.github.com>
2023-07-04 00:20:30 -0300

    Add files via upload

--- src/outros/sticker.js
+++ src/outros/sticker.js
@@ -1,12 +1,12 @@
-async function sticker(legendaFoto, downloadMediaMessage, msg, logger, sock, fs, exec, jid, msg, fs){
+async function sticker(legendaFoto, downloadMediaMessage, msg, logger, sock, fs, sharp, jid, msg, fs){
     if(legendaFoto == "/sticker"){
         const buffer = await downloadMediaMessage(msg, 'buffer', { }, { logger, reuploadRequest: sock.updateMediaMessage})
         await fs.writeFileSync('./data-base/media/sticker/sticker.png', buffer)
-        await exec('cwebp -q 80 ./data-base/media/sticker/sticker.png -o ./data-base/media/sticker/sticker.webp');
+        sharp('./data-base/media/sticker/sticker.png').webp().toFile('./data-base/media/sticker/sticker.webp').then(() => console.log('Imagem convertida com sucesso!')).catch((err) => console.error(err));
         sock.sendMessage(jid, {text: "criando figurinha..."}, {quoted:msg})
         setTimeout(() => {
         	if(fs.existsSync("./data-base/media/sticker/sticker.webp")){
-                sock.sendMessage( jid, { sticker: { url: `./data-base/media/sticker/sticker.webp` }, mimetype: 'image/webp' , caption: `voce `}, { pack: 'meu-pacote', author: 'meu-nome', quoted:msg })
+                sock.sendMessage( jid, { sticker: { url: `./data-base/media/sticker/sticker.webp` }, mimetype: 'image/webp' , caption: `psc`}, { quoted:msg })
             }
         }, 5000);
     }
